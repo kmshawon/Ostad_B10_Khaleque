117e534d935cef216c78d5187919d9f56281b13b
MD ABDUL KHALEQUE <kmshawon01850160659@gmail.com>
2025-04-06 00:38:37 +0600

    First Setup
    SetUp

---
+++ OOP/Class.dart
@@ -0,0 +1,23 @@
+
+import 'Human.dart';
+void main() {
+  //print('Hello, World!');
+  Human Khaleque =Human();
+  Khaleque.name='Khaleque';
+  Khaleque.color='Brown';
+
+  print(Khaleque.name);
+  Khaleque.moving();
+  Khaleque.sleep();
+
+}
+class Student {
+  String ? StudentName;
+  String ? StudentClass;
+  String ? StudentAddress;
+
+
+
+}
+
+
---
+++ OOP/Human.dart
@@ -0,0 +1,26 @@
+class Human{
+  int legs =2;
+  int Hands=2;
+  String ? color;
+  int eye=2;
+  String? name;
+  static String classname='Human Class';
+
+  Human(){
+    print('Human Object Created');
+
+  }
+
+  moving(){
+    print('$name is movig');
+
+  }
+  method1(){
+    print('Method 1');
+
+  }
+  sleep(){
+    print('$name is Sleeping');
+
+  }
+}
